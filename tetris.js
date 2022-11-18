2b3d2a9d639b3bd0163633b7f43bf979366ccb52
Hirake <Robin.berg@hotmail.nl>
2022-11-18 18:20:26 +0100

    single block falls down and respawns at top when hitting bottom

--- tetris.js
+++ tetris.js
@@ -5,37 +5,60 @@ const mapWidth = 400;
 const mapHeight = 800;
 const mapPadding = 10;
 
-const mapBlockWidth = 9;
-const mapBlockHeight = 18;
+const blockWidth = 9;
+const blockHeight = 18;
+const blockPadding = mapHeight/blockHeight;
+
+let position = -1;
 
 function drawMap(){    
-    for (let x = 0; x <= mapWidth * 2 + 1; x+= mapHeight/mapBlockHeight) {
+    for (let x = 0.5; x <= mapWidth * 2 + 1; x+= mapHeight/blockHeight) {
         ctx.moveTo(mapPadding, x + mapPadding);
         ctx.lineTo(mapWidth + mapPadding, x + mapPadding);       
     }
 
-    for (let y = 0.5; y <= mapHeight / 2 + 1; y+= mapWidth/mapBlockWidth) {
+    for (let y = 0.5; y <= mapHeight / 2 + 1; y+= mapWidth/blockWidth) {
         ctx.moveTo(y + mapPadding, mapPadding);
         ctx.lineTo(y + mapPadding, mapHeight + mapPadding);       
     }
     
 }
 
+function gameLoop(){
+    drawMap();
+    setInterval(updateMap, 1000/2)
+    
+}
+
+function drawShape(xPosition){
+    ctx.fillStyle = "red";
+    ctx.fillRect(mapPadding + 0.5, mapPadding + 0.5 + xPosition, blockPadding, blockPadding);
+}
+
+function updateMap(){   
+    if (position >= blockHeight - 1) {
+        position = -1;
+    }
+    ctx.clearRect(mapPadding + 1, mapPadding + 1, mapWidth - 1, mapHeight - 1)
+    drawMap();
+    position++;
+    drawShape(position * blockPadding);
+    
+}
+
 drawMap();
+gameLoop();
 
 ctx.strokeStyle = "black";
 ctx.stroke();
 
-    drawMap();
-
-    class Block {
+class Block {
     constructor(x, y, template){
         this.x = x;
         this.y = y;
         this.template = template;
     }
 }
-
 const blocks = [
     new Block(0, 120, [
         [0, 1, 0],
